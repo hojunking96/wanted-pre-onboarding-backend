0181f892348b5301e53715e309eb1f8b374b683a
hojunking96 <hojunking96@gmail.com>
2023-08-07 00:43:58 +0900

    Refactor : Post 응답 형식 변경

---
+++ src/test/java/api/wantedpreonboardingbackend/domain/post/controller/PostControllerTest.java
@@ -0,0 +1,28 @@
+package api.wantedpreonboardingbackend.domain.post.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostControllerTest {
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void getPosts() {
+    }
+
+    @Test
+    void getPost() {
+    }
+
+    @Test
+    void modify() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
